a5a184b7238fddd05c402d9597ab827eae31b2ff
pau1838 <pau.ibanez@hotmail.com>
2020-03-27 17:55:20 +0100

    Starting models created

--- apps/reservations/models.py
+++ apps/reservations/models.py
@@ -1,3 +1,33 @@
+from django import forms
 from django.db import models
 
-# Create your models here.
+
+class Sport(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Installation(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField()
+    capacity = models.IntegerField()
+    sports = models.ManyToManyField(Sport, on_delete=models.SET_NULL, related_name='installations')
+
+
+class Organizer(models.Model):
+    num_client = models.CharField(max_length=30)
+    password = models.CharField(max_length=30, widget=forms.PasswordInput)
+
+
+class CurrentReservations(models.Model):
+    organizer = models.ForeignKey(Organizer, on_delete=models.CASCADE, related_name='current_reservations')
+
+
+class Reservation(models.Model):
+    date = models.DateField()
+    start_hour = models.TimeField()
+    end_hour = models.TimeField()
+    organizer = models.ForeignKey(Organizer, on_delete=models.CASCADE, related_name='reservations')
+    installation = models.ForeignKey(Installation, on_delete=models.CASCADE, related_name='reservations')
+    current_reservations = models.ForeignKey(CurrentReservations, on_delete=models.SET_NULL,
+                                             related_name='reservations', null=True)
