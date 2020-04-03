945fdf5fa0363348da24b62e1b32d338248894a8
pau1838 <pau.ibanez@hotmail.com>
2020-04-03 10:31:31 +0200

    Models updated

--- apps/reservations/admin.py
+++ apps/reservations/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Installation
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Installation)
+admin.site.register(Sport)
+admin.site.register(CurrentReservations)
+admin.site.register(Date)
+admin.site.register(Reservation)
--- apps/reservations/models.py
+++ apps/reservations/models.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,24 +5,43 @@ from django.contrib.auth.models import User
 class Sport(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class Installation(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(blank=True)
     capacity = models.IntegerField()
     sports = models.ManyToManyField(Sport, related_name='installations')
+
+    def __str__(self):
+        return self.name
 
 
 class CurrentReservations(models.Model):
     organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='current_reservations')
 
+    def __str__(self):
+        return 'Reserves de la sessió actual de ' + self.organizer.username + ' ' + self.organizer.username
 
-class Reservation(models.Model):
-    date = models.DateField()
+
+class Date(models.Model):
+    day = models.DateField()
     start_hour = models.TimeField()
     end_hour = models.TimeField()
+
+    def __str__(self):
+        return "Dia " + str(self.day) + " hora " + str(self.start_hour) + '-' + str(self.end_hour)
+
+
+class Reservation(models.Model):
+    date = models.ForeignKey(Date, on_delete=models.SET_NULL, null=True, related_name='reservation')
     organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reservations')
     installation = models.ForeignKey(Installation, on_delete=models.CASCADE, related_name='reservations')
     current_reservations = models.ForeignKey(CurrentReservations, on_delete=models.SET_NULL,
-                                             related_name='reservations', null=True)
+                                             related_name='reservations', null=True, blank=True)
+
+    def __str__(self):
+        return "Reserva de " + self.organizer.username
