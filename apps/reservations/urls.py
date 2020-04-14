5ff6d4496cc62995a15f9b766d51f083db68f47a
Gerard <gerardgg02@gmail.com>
2020-04-14 17:24:35 +0200

    Filter applied to Checkout-bill

--- apps/reservations/urls.py
+++ apps/reservations/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from . import views
 from django.contrib import admin
+from django.urls import path
+from apps.reservations import views
 
 urlpatterns = [
-    path('', views.show_installations, name='index'),
-    path('<str:sport>', views.filtered_installations, name='filtered_index'),
+    path('show_installations/', views.show_installations, name='index'),
+    path('show_installations_reserved/<str:username>', views.show_installations_reserved, name='installations_reserved'),
+    path('reservations/<int:pk_inst>/date/<str:current_date>', views.reserve_day_hours, name='reservations'),
+    path('show_installations_reserved/<str:username>/checkout', views.checkout, name='checkout'),
+    path('change_date/<int:pk_inst>', views.change_date, name='change_date'),
+    path('cancel_reserve/<int:pk_reserve>', views.delete_reserve, name='delete_reserve'),
+    path('show_installations/<str:sport>', views.filtered_installations, name='filtered_index'),
 ]
