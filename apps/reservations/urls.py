bd43eb37f63e20a7893d6e1f5cce4f7fe25d3c1e
Gerard <gerardgg02@gmail.com>
2020-04-14 15:36:05 +0200

    Sports filter for installation_list.html applied

--- apps/reservations/urls.py
+++ apps/reservations/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', views.show_installations, name='index'),
+    path('<str:sport>', views.filtered_installations, name='filtered_index'),
 ]
--- apps/reservations/views.py
+++ apps/reservations/views.py
@@ -4,10 +4,18 @@ from .models import Installation
 
 # Create your views here.
 def show_installations(request):
-
     installations = Installation.objects.order_by('sports')
     context = {
-        'installations' : installations,
+        'installations': installations,
+    }
+
+    return render(request, 'installation_list.html', context)
+
+
+def filtered_installations(request, sport):
+    installations = Installation.objects.filter(sports__name=sport)
+    context = {
+        'installations': installations,
     }
 
     return render(request, 'installation_list.html', context)
