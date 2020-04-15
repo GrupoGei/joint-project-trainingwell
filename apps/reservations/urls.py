81f2ad667c4451640348cd011d0b4642b4ea3b7a
Gerard <gerardgg02@gmail.com>
2020-04-15 23:11:25 +0200

    Login implemented

--- apps/reservations/urls.py
+++ apps/reservations/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from apps.reservations import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('show_installations/', views.show_installations, name='index'),
     path('show_installations_reserved/<str:username>', views.show_installations_reserved, name='installations_reserved'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('change_date/<int:pk_inst>', views.change_date, name='change_date'),
     path('cancel_reserve/<int:pk_reserve>', views.delete_reserve, name='delete_reserve'),
     path('show_installations/<str:sport>', views.filtered_installations, name='filtered_index'),
+    path('', auth_views.LoginView.as_view(template_name= 'login.html'), name='login')
 ]
